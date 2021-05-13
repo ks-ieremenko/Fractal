2815c2f7a2fb498c6becabadd08c53bea50363ab
Kseniya Ieremenko <keremenko36@gmail.com>
2021-05-13 23:49:54 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Fractal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Fractal_1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Fractal_2 = new System.Windows.Forms.Button();
+            this.FractalPictureBox = new System.Windows.Forms.PictureBox();
+            this.clear_Button = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.FractalPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Fractal_1
+            // 
+            this.Fractal_1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Fractal_1.Location = new System.Drawing.Point(44, 52);
+            this.Fractal_1.Name = "Fractal_1";
+            this.Fractal_1.Size = new System.Drawing.Size(183, 71);
+            this.Fractal_1.TabIndex = 0;
+            this.Fractal_1.Text = "Треугольник Серпинского";
+            this.Fractal_1.UseVisualStyleBackColor = true;
+            this.Fractal_1.Click += new System.EventHandler(this.Fractal_1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14F);
+            this.label1.Location = new System.Drawing.Point(372, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выберите фрактал";
+            // 
+            // Fractal_2
+            // 
+            this.Fractal_2.Font = new System.Drawing.Font("Times New Roman", 14F);
+            this.Fractal_2.Location = new System.Drawing.Point(772, 52);
+            this.Fractal_2.Name = "Fractal_2";
+            this.Fractal_2.Size = new System.Drawing.Size(197, 71);
+            this.Fractal_2.TabIndex = 2;
+            this.Fractal_2.Text = "Снежинка Коха";
+            this.Fractal_2.UseVisualStyleBackColor = true;
+            this.Fractal_2.Click += new System.EventHandler(this.Fractal_2_Click);
+            // 
+            // FractalPictureBox
+            // 
+            this.FractalPictureBox.Location = new System.Drawing.Point(267, 171);
+            this.FractalPictureBox.Name = "FractalPictureBox";
+            this.FractalPictureBox.Size = new System.Drawing.Size(466, 393);
+            this.FractalPictureBox.TabIndex = 3;
+            this.FractalPictureBox.TabStop = false;
+            // 
+            // clear_Button
+            // 
+            this.clear_Button.Location = new System.Drawing.Point(319, 597);
+            this.clear_Button.Name = "clear_Button";
+            this.clear_Button.Size = new System.Drawing.Size(320, 23);
+            this.clear_Button.TabIndex = 5;
+            this.clear_Button.Text = "Стереть все";
+            this.clear_Button.UseVisualStyleBackColor = true;
+            this.clear_Button.Click += new System.EventHandler(this.clear_Button_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(76, 151);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(809, 151);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 7;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1013, 632);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.clear_Button);
+            this.Controls.Add(this.FractalPictureBox);
+            this.Controls.Add(this.Fractal_2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Fractal_1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.FractalPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Fractal_1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Fractal_2;
+        private System.Windows.Forms.PictureBox FractalPictureBox;
+        private System.Windows.Forms.Button clear_Button;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fractal
+{
+    public partial class Form1 : Form
+    {
+        public int level;
+        //Высота и ширина для отрисовки
+        private int width, height, i;
+        // Bitmap для фрактала
+        private Bitmap fractal;
+        // используем для отрисовки на PictureBox
+        private Graphics graph;
+        PointF topPoint, leftPoint, rightPoint, downPoint;
+        static Pen pen1, pen2;
+        public Form1()
+        {
+            InitializeComponent();
+            width = FractalPictureBox.Width;
+            height = FractalPictureBox.Height;
+        }
+
+        private void Fractal_1_Click(object sender, EventArgs e)
+        {
+            fractal = new Bitmap(width, height);
+            // cоздаем новый объект Graphics из указанного Bitmap
+            graph = Graphics.FromImage(fractal);
+            graph.SmoothingMode = SmoothingMode.HighQuality;
+            topPoint = new PointF(width / 2f, 0);
+            leftPoint = new PointF(0, height);
+            rightPoint = new PointF(width, height);
+            DrawTriangle(0, topPoint, leftPoint, rightPoint);
+            FractalPictureBox.BackgroundImage = fractal;
+        }
+        private void Fractal_2_Click(object sender, EventArgs e)
+        {
+            fractal = new Bitmap(width, height);
+            graph = Graphics.FromImage(fractal);
+            graph.SmoothingMode = SmoothingMode.HighQuality;
+            downPoint = new PointF(width / 2f, height);
+            leftPoint = new PointF(0, height / 4f);
+            rightPoint = new PointF(width, height / 4f);
+            pen1 = new Pen(Color.Blue, 1);
+            pen2 = new Pen(Color.White, 2f);
+            //Зарисуем треугольник
+            graph.DrawLine(pen1, leftPoint, rightPoint);
+            graph.DrawLine(pen1, rightPoint, downPoint);
+            graph.DrawLine(pen1, downPoint, leftPoint);
+            FractalPictureBox.BackgroundImage = fractal;
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            level = Convert.ToInt32(numericUpDown1.Value);
+            fractal = new Bitmap(width, height);
+            graph = Graphics.FromImage(fractal);
+            graph.SmoothingMode = SmoothingMode.HighQuality;
+            DrawTriangle(level, topPoint, leftPoint, rightPoint);
+            FractalPictureBox.BackgroundImage = fractal;
+        }
+
+        
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            i = Convert.ToInt32(numericUpDown2.Value);
+            fractal = new Bitmap(width, height);
+            graph = Graphics.FromImage(fractal);
+            graph.SmoothingMode = SmoothingMode.HighQuality;
+            graph.DrawLine(pen1, leftPoint, rightPoint);
+            graph.DrawLine(pen1, rightPoint, downPoint);
+            graph.DrawLine(pen1, downPoint, leftPoint);
+            Fractal(leftPoint, rightPoint, downPoint, i);
+            Fractal(rightPoint, downPoint, leftPoint, i);
+            Fractal(downPoint, leftPoint, rightPoint, i);
+            FractalPictureBox.BackgroundImage = fractal;
+        }
+
+        int Fractal(PointF p1, PointF p2, PointF p3, int iter)
+        {
+            if (iter > 0)  //условие выхода из рекурсии
+            {
+                //средняя треть отрезка
+                var p4 = new PointF((2*p1.X + p2.X) / 3, (2*p1.Y + p2.Y) / 3);
+                var p5 = new PointF((2*p2.X + p1.X) / 3, (p1.Y + 2*p2.Y) / 3);
+                //координаты вершины угла
+                var ps = MidPoint(p2, p1);
+                var pn = new PointF((4 * ps.X - p3.X) / 3, (4 * ps.Y - p3.Y) / 3);
+                //рисуем его
+                graph.DrawLine(pen1, p4, pn);
+                graph.DrawLine(pen1, p5, pn);
+                graph.DrawLine(pen2, p4, p5);
+                graph.DrawLine(pen2, p4, p5);
+                //рекурсивно вызываем функцию нужное число раз
+                Fractal(p4, pn, p5, iter - 1);
+                Fractal(pn, p5, p4, iter - 1);
+                Fractal(p1, p4, new PointF((2 * p1.X + p3.X) / 3, (2 * p1.Y + p3.Y) / 3), iter - 1);
+                Fractal(p5, p2, new PointF((2 * p2.X + p3.X) / 3, (2 * p2.Y + p3.Y) / 3), iter - 1);
+            }
+            return iter;
+        }
+        private void DrawTriangle(int iter, PointF top, PointF left, PointF right)
+        {
+            //проверяем, закончили ли мы построение
+            if (iter == 0)
+            {
+                PointF[] points = new PointF[3] { top, right, left };
+                //рисуем  треугольник
+                graph.FillPolygon(Brushes.DarkOrange, points);
+            }
+            else
+            {
+                //вычисляем среднюю точку
+                var leftMid = MidPoint(top, left); //левая сторона
+                var rightMid = MidPoint(top, right); //правая сторона
+                var topMid = MidPoint(left, right); // основание
+                //рекурсивно вызываем функцию для каждого и 3 треугольников
+                DrawTriangle(iter - 1, top, leftMid, rightMid);
+                DrawTriangle(iter - 1, leftMid, left, topMid);
+                DrawTriangle(iter - 1, rightMid, topMid, right);
+            }
+        }
+        private PointF MidPoint(PointF p1, PointF p2)
+        {
+            return new PointF((p1.X + p2.X) / 2f, (p1.Y + p2.Y) / 2f);
+        }
+        private void clear_Button_Click(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = 0;
+            numericUpDown2.Value = 0;
+            FractalPictureBox.BackgroundImage = null;
+        }
+    }
+}
